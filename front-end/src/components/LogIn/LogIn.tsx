ba87ca44d1a82efb552093f063531ddb86467d25
Bogya Roland <roy.bogya@gmail.com>
2021-09-26 12:43:27 +0200

    New color added, setup

--- front-end/src/components/LogIn/LogIn.tsx
+++ front-end/src/components/LogIn/LogIn.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const LogIn = () => {
   return (
     <div>
-      <h1>Log in</h1>
+      <h1 className="m-4 text-verybig hover:text-oeyellow">Log in</h1>
     </div>
   );
 };
--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -2,7 +2,14 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        'verybig': "120px"
+      },
+      textColor: {
+        'oeyellow': '#FAB001',
+      }
+    },
   },
   variants: {
     extend: {},
