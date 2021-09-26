0793eef73a8ff39bff43dd9cb98d2cca98d6c796
Bence Papp <bencepapp1101@gmail.com>
2021-09-26 09:07:36 +0200

    redux store set up

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,14 +1,6 @@
-import axios from "./axios";
-import React, { useEffect } from "react";
+import React from "react";
 
 function App() {
-  useEffect(() => {
-    axios
-      .get("/")
-      .then((res) => console.log(res))
-      .catch((err) => console.log(err));
-  }, []);
-
   return (
     <div>
       <h1 className="m-4 text-8xl font-light">Foerum</h1>
