a0ceb5762c21b44e9db67471e1c1cc7d53ab0a85
Boldi Bihari <boldibihari@gmail.com>
2021-10-03 12:10:13 +0200

    AwardRepository getall done

--- back-end/Repository/Class/AwardRepository.cs
+++ back-end/Repository/Class/AwardRepository.cs
@@ -31,7 +31,7 @@ namespace Repository.Class
 
         public IQueryable<Award> GetAll()
         {
-            throw new NotImplementedException();
+            return this.db.Set<Award>();
         }
 
         public Award GetOne(string id)
