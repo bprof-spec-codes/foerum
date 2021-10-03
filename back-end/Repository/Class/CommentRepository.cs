21bae7cac40c8ce8db8448df508ff6f0b080579d
Boldi Bihari <boldibihari@gmail.com>
2021-10-03 12:23:01 +0200

    CommentRepository delete done

--- back-end/Repository/Class/CommentRepository.cs
+++ back-end/Repository/Class/CommentRepository.cs
@@ -25,7 +25,8 @@ namespace Repository.Class
 
         public void Delete(string id)
         {
-            throw new NotImplementedException();
+            this.db.Set<Comment>().Remove(this.GetOne(id));
+            this.db.SaveChanges();
         }
 
         public IQueryable<Comment> GetAll()
