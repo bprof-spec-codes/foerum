e1ff567e397c77aaa443d603687ff64ffc5f84dc
Boldi Bihari <boldibihari@gmail.com>
2021-10-03 15:18:14 +0200

    TagRepository add done

--- back-end/Repository/Class/TagRepository.cs
+++ back-end/Repository/Class/TagRepository.cs
@@ -9,37 +9,38 @@ using System.Threading.Tasks;
 
 namespace Repository.Class
 {
-      public class TagRepository : ITagRepository
-      {
-            public FoerumDbContext db;
+    public class TagRepository : ITagRepository
+    {
+        public FoerumDbContext db;
 
-            public TagRepository(string dbPassword)
-            {
-                  this.db = new FoerumDbContext(dbPassword);
-            }
-            public void Add(Tag tag)
-            {
-                  throw new NotImplementedException();
-            }
+        public TagRepository(string dbPassword)
+        {
+            this.db = new FoerumDbContext(dbPassword);
+        }
+        public void Add(Tag tag)
+        {
+            this.db.Set<Tag>().Add(tag);
+            this.db.SaveChanges();
+        }
 
-            public void Delete(string id)
-            {
-                  throw new NotImplementedException();
-            }
+        public void Delete(string id)
+        {
+            throw new NotImplementedException();
+        }
 
-            public IQueryable<Tag> GetAll()
-            {
-                  throw new NotImplementedException();
-            }
+        public IQueryable<Tag> GetAll()
+        {
+            throw new NotImplementedException();
+        }
 
-            public Tag GetOne(string id)
-            {
-                  throw new NotImplementedException();
-            }
+        public Tag GetOne(string id)
+        {
+            throw new NotImplementedException();
+        }
 
-            public void Update(string id, Tag tag)
-            {
-                  throw new NotImplementedException();
-            }
-      }
+        public void Update(string id, Tag tag)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
