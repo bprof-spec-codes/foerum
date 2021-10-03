5f1fa8e99ff32ca97a4248c2718cee5aaf86b923
Boldi Bihari <boldibihari@gmail.com>
2021-10-03 15:30:13 +0200

    TopicRepository add done

--- back-end/Repository/Class/TopicRepository.cs
+++ back-end/Repository/Class/TopicRepository.cs
@@ -9,37 +9,38 @@ using System.Threading.Tasks;
 
 namespace Repository.Class
 {
-      public class TopicRepository : ITopicRepository
-      {
-            public FoerumDbContext db;
+    public class TopicRepository : ITopicRepository
+    {
+        public FoerumDbContext db;
 
-            public TopicRepository(string dbPassword)
-            {
-                  this.db = new FoerumDbContext(dbPassword);
-            }
-            public void Add(Topic topic)
-            {
-                  throw new NotImplementedException();
-            }
+        public TopicRepository(string dbPassword)
+        {
+            this.db = new FoerumDbContext(dbPassword);
+        }
+        public void Add(Topic topic)
+        {
+            this.db.Set<Topic>().Add(topic);
+            this.db.SaveChanges();
+        }
 
-            public void Delete(string id)
-            {
-                  throw new NotImplementedException();
-            }
+        public void Delete(string id)
+        {
+            throw new NotImplementedException();
+        }
 
-            public IQueryable<Topic> GetAll()
-            {
-                  throw new NotImplementedException();
-            }
+        public IQueryable<Topic> GetAll()
+        {
+            throw new NotImplementedException();
+        }
 
-            public Topic GetOne(string id)
-            {
-                  throw new NotImplementedException();
-            }
+        public Topic GetOne(string id)
+        {
+            throw new NotImplementedException();
+        }
 
-            public void Update(string id, Topic topic)
-            {
-                  throw new NotImplementedException();
-            }
-      }
+        public void Update(string id, Topic topic)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
