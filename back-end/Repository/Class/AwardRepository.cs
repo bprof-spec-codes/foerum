0812e7c36ad8bc68af8f689e3fb25a648ebc004e
Boldi Bihari <boldibihari@gmail.com>
2021-10-03 12:07:54 +0200

    AwardRepository add done

--- back-end/Repository/Class/AwardRepository.cs
+++ back-end/Repository/Class/AwardRepository.cs
@@ -9,37 +9,38 @@ using System.Threading.Tasks;
 
 namespace Repository.Class
 {
-      public class AwardRepository : IAwardRepository
-      {
-            public FoerumDbContext db;
+    public class AwardRepository : IAwardRepository
+    {
+        public FoerumDbContext db;
 
-            public AwardRepository(string dbPassword)
-            {
-                  this.db = new FoerumDbContext(dbPassword);
-            }
-            public void Add(Award award)
-            {
-                  throw new NotImplementedException();
-            }
+        public AwardRepository(string dbPassword)
+        {
+            this.db = new FoerumDbContext(dbPassword);
+        }
+        public void Add(Award award)
+        {
+            this.db.Set<Award>().Add(award);
+            this.db.SaveChanges();
+        }
 
-            public void Delete(string id)
-            {
-                  throw new NotImplementedException();
-            }
+        public void Delete(string id)
+        {
+            throw new NotImplementedException();
+        }
 
-            public IQueryable<Award> GetAll()
-            {
-                  throw new NotImplementedException();
-            }
+        public IQueryable<Award> GetAll()
+        {
+            throw new NotImplementedException();
+        }
 
-            public Award GetOne(string id)
-            {
-                  throw new NotImplementedException();
-            }
+        public Award GetOne(string id)
+        {
+            throw new NotImplementedException();
+        }
 
-            public void Update(string id, Award award)
-            {
-                  throw new NotImplementedException();
-            }
-      }
+        public void Update(string id, Award award)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
