48ae4016650ebf5d915c6df555f5bbcf3bb0b58d
conoren <conoren@gmail.com>
2021-10-08 16:25:06 +0200

    made a good looking login screen

--- front-end/src/components/LogIn/LogIn.tsx
+++ front-end/src/components/LogIn/LogIn.tsx
@@ -1,53 +1,42 @@
-import React, { FC, useState } from "react";
-import { connect } from "react-redux";
-import { RouteComponentProps } from "react-router-dom";
-import { IRootState } from "src/store/reducers";
-import { login } from "../../store/reducers/authentication";
+import React from 'react';
+import './login.scss';
+import logo from '../../assets/images/logo.png'
 
-export interface ILoginProps
-  extends StateProps,
-    DispatchProps,
-    RouteComponentProps<{}> {}
+const Login = () =>{
 
-const LogIn: FC<ILoginProps> = (props) => {
-  const [userName, setUserName] = useState<string>("");
-  /* create password based on username here */
-
-  const handleSubmit = () => {
-    /* u should call props.login inside here */
-  };
 
   return (
-    <div>
-      <div>
-        {/* this div is the left sido of the login page(figma) */}
-        <div></div>
-
-        {/* this div is the left sido of the login page(figma) */}
-        <div>
-          {/* u shloud create a form inside here(u can use form tag with inputs and submit button(recommended) or a simple div with inputs) */}
-          <form onSubmit={handleSubmit}>
-            <input
-              type="text"
-              value={userName}
-              onChange={(e) => setUserName(e.target.value)}
-            />
-            <button type="submit">button</button>
-          </form>
+    <div className="login">
+        <div className="login__colored-container">
+          <img className="login__colored-container__logo-container--image" src={logo} alt="Budwriter" />
         </div>
-      </div>
+        <div className={`login__login-container ${true ? 'login__login-container--active' : 'login__login-container--inactive'}`}>
+            <div className="login__login-container__main-container">
+                Kérlek jelentkezz be
+                <div className="login__login-container__main-container__form-container">
+                    <form className="login__login-container__main-container__form-container__form" onSubmit={(e) => {
+                        e.preventDefault();
+                    }}>
+                        <input
+                            className="login__login-container__main-container__form-container__form--email"
+                            type="email"
+                            placeholder="Email"
+                            required />
+                        <input
+                            className="login__login-container__main-container__form-container__form--password"
+                            type="password"
+                            placeholder="Password"
+                            required />
+                        <button
+                            className="login__login-container__main-container__form-container__form--submit">
+                            Bejelentkezés
+                    </button>
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
-  );
-};
+);
+}
 
-const mapStateToProps = ({ authentication }: IRootState) => ({
-  isAuthenticated: authentication.isAuthenticated,
-  loginError: authentication.loginError,
-});
-
-const mapDispatchToProps = { login };
-
-type StateProps = ReturnType<typeof mapStateToProps>;
-type DispatchProps = typeof mapDispatchToProps;
-
-export default connect(mapStateToProps, mapDispatchToProps)(LogIn);
+export default Login;
