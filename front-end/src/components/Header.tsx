a6b3b59a7fa93e5768c05f0dd13d4b486f3be9c8
conoren <conoren@gmail.com>
2021-10-08 17:34:08 +0200

    made a nicer header

--- front-end/src/components/Header.tsx
+++ front-end/src/components/Header.tsx
@@ -1,21 +1,21 @@
 import React from "react";
+import minilogo from '../assets/images/minilogo.png'
+import "./Home/home.scss"
 
 const Header = () => {
   return (
     <div className="h-16 bg-basebg">
       <div className="flex justify-between h-full content-center text-center text-white">
-        <div className="flex content-center p-5">
-          <img src="" alt="Logo" loading="lazy" />
-        </div>
+        <img src={minilogo} alt="Logo" loading="lazy" />
 
-        <div className="flex w-10/12 p-4">
+        <div className="flex w-10/12 p-4  px-20">
             <div className="flex w-full h-8 bg-white rounded-2xl">
-            <input type="text" className="flex w-full my-2 mx-4 text-black outline-none" placeholder={`Search`} />
+            <input type="text" className="flex w-full my-2 mx-4 text-black outline-none bg-white" placeholder={`Keresés`} />
             </div>
         </div>
 
         <div className="flex content-center p-5">
-          <h1>Jobb</h1>
+          <h1>Admin</h1>
         </div>
       </div>
     </div>
