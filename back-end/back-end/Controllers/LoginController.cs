352255a28b609064eca8366dee53845c741dd684
Roland Bogdan <bogdanroland07@gmail.com>
2021-10-10 15:39:35 +0200

    Api endpoint settings done, logincontroller created, fixed data layer being nunit test

---
+++ back-end/back-end/Controllers/LoginController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using System.Threading.Tasks;
+
+namespace back_end.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class LoginController
+    {
+    }
+}
