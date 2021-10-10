5be3965bef69419fff5d3527a6efbb1d0e3ec948
Roland Bogdan <bogdanroland07@gmail.com>
2021-10-10 15:52:07 +0200

    Logincontroller done

--- back-end/back-end/Controllers/LoginController.cs
+++ back-end/back-end/Controllers/LoginController.cs
@@ -10,7 +10,32 @@ namespace back_end.Controllers
 {
     [Route("[controller]")]
     [ApiController]
-    public class LoginController
+    public class LoginController : ControllerBase
     {
+        [AllowAnonymous]
+        [HttpGet]
+        public IActionResult IsUserLoggedIn()
+        {
+            if (!this.HttpContext.User.Identity.IsAuthenticated)
+            {
+                return this.Unauthorized();
+            }
+            return this.Accepted();
+        }
+
+        [AllowAnonymous]
+        [HttpGet("Authenticate")]
+        public async Task Login()
+        {
+            if (!this.HttpContext.User.Identity.IsAuthenticated)
+            {
+                this.HttpContext.Items.Add("allowRedirect", true);
+                await this.HttpContext.ChallengeAsync();
+                return;
+            }
+
+            // TODO URL change
+            this.HttpContext.Response.Redirect("http://localhost:8585/");
+        }
     }
 }
