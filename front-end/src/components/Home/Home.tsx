2d48687da43eddefbc9df208ae8f3b3da204d960
conoren <conoren@gmail.com>
2021-10-18 19:53:02 +0200

    removed things i dont understand

--- front-end/src/components/Home/Home.tsx
+++ front-end/src/components/Home/Home.tsx
@@ -1,26 +1,33 @@
 import React from "react";
-<<<<<<< Updated upstream
 import Feed from "./Feed";
 import ProfileActions from "./ProfileActions";
 import Sidebar from "./Sidebar";
+import Header from "./Header";
 
 const Home = () => {
   return (
-    <div className="flex m-5">
-      <div className="flex justify-between w-full">
-        <div className="w-1/3 mr-5">
-          <Sidebar />
-        </div>
+    <div>
+      <Header/>
+      <div className="flex m-5">
+        <div className="flex justify-between w-full">
+          <div className="w-1/3 mr-5">
+            <Sidebar />
+          </div>
 
-        <div className="w-1/3 mr-5">
-          <Feed />
-        </div>
+          <div className="w-1/3 mr-5">
+            <Feed />
+          </div>
 
-        <div className="w-1/3">
-          <ProfileActions />
+          <div className="w-1/3">
+            <ProfileActions />
+          </div>
         </div>
       </div>
-=======
+
+    </div>
+  )
+};
+/*
 import Header from "./Header";
 import "./home.scss"
 import Topic from "../Misc/Topic";
@@ -37,5 +44,5 @@ const Home = () => {
     
   );
 };
-
+*/
 export default Home;
