69d68f0047b9df82319e665bcff35fdfd2dd9c51
Tamás Pap <tamas.pap@bitriderz.com>
2021-10-19 10:49:41 -0700

    comment removed

--- back-end/back-end/Startup.cs
+++ back-end/back-end/Startup.cs
@@ -123,7 +123,6 @@ namespace back_end
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
