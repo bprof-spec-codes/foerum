e1b884becd7883979b387f4cacc97b9bb8c86cea
Boldi Bihari <boldibihari@gmail.com>
2021-11-01 19:54:40 +0100

    Models updated

--- back-end/Models/SubjectUsers.cs
+++ back-end/Models/SubjectUsers.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Models
@@ -14,8 +15,10 @@ namespace Models
         //[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         //public string SubjectUsersID { get; set; }
         public string SubjectID { get; set; }
+        [JsonIgnore]
         public Subject Subject { get; set; }
         public string UserID { get; set; }
+        [JsonIgnore]
         public MyUser User { get; set; }
     }
 }
