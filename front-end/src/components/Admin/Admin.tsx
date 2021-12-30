b62aea81fe26d04904923bb78633320aebb1784f
bencepapp1101 <bencepapp1101@gmail.com>
2021-12-30 17:49:18 +0100

    admin page sidebar created

--- front-end/src/components/Admin/Admin.tsx
+++ front-end/src/components/Admin/Admin.tsx
@@ -1,52 +1,100 @@
 import axios from "../../axios";
 import React, { useEffect, useState } from "react";
+import s from "./Admin.module.scss";
 import { IUser } from "src/models/user.model";
 import Header from "../Home/Header";
 
+import PermIdentityOutlinedIcon from "@mui/icons-material/PermIdentityOutlined";
+import PaidOutlinedIcon from "@mui/icons-material/PaidOutlined";
+import GridViewOutlinedIcon from "@mui/icons-material/GridViewOutlined";
+import LocalFireDepartmentOutlinedIcon from "@mui/icons-material/LocalFireDepartmentOutlined";
+
 const Admin = () => {
   const [users, setUsers] = useState<IUser[] | null>(null);
+  const [actualPage, setActualPage] = useState<number>(1);
+
   useEffect(() => {
-    axios
-      .get<IUser[]>("/MyUser")
-      .then((respone) => {
-        console.log(respone.data);
-        setUsers(respone.data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    getUsers();
   }, []);
 
-  return (
-    <div>
-      <Header />
-      <div className="flex-col my-12 w-full">
-        <div className="flex justify-between bg-green-200 rounded mx-24 p-5">
-          <div>adatok</div>
-          <div>kép</div>
-        </div>
-        <div className="flex-col mx-24">
-          {users &&
-            users.map((user, i) => (
-              <div
-                key={i}
-                className="flex bg-blue-200 justify-between p-5 my-2 rounded cursor-pointer"
-              >
-                <div className="w-1/2">
-                  <h3>Név: {user.fullName}</h3>
-                  <p>Role: {user.role}</p>
-                  <p className="text-xs">{user.email}</p>
-                </div>
-                <div className="w-1/2">
-                  <p>Kezdés éve: {user.startYear}</p>
-                  <p>NIK Coin: {user.nikCoin}</p>
-                  <p>Aktív: {user.isActive ? "Aktív" : "Inaktív"}</p>
-                </div>
+  const getUsers = async () => {
+    const { data } = await axios.get<IUser[]>("/MyUser");
+    console.log(data);
+    setUsers(data);
+  };
+
+  const getComments = async () => {
+    const { data } = await axios.get<IUser[]>("/MyUser");
+    console.log(data);
+    setUsers(data);
+  };
+
+  const renderMain = (): JSX.Element => {
+    switch (actualPage) {
+      case 2:
+      case 3:
+      case 4:
+      default:
+        return (
+          <>
+            <div className={s.hotTopics}>
+              <div className={s.topic}>
+                <h2>topic</h2>
               </div>
-            ))}
+              <div className={s.topic}>
+                <h2>topic</h2>
+              </div>
+              <div className={s.topic}>
+                <h2>topic</h2>
+              </div>
+            </div>
+
+            <div className={s.latestActivity}>
+              <h3>Latest Activity</h3>
+            </div>
+          </>
+        );
+    }
+  };
+
+  return (
+    <>
+      <Header />
+      <div className={s.root}>
+        <div className={s.sidebar}>
+          <ul className={s.sidebarList}>
+            <li className={s.sidebarListItem} onClick={() => setActualPage(1)}>
+              <div className={actualPage === 1 ? s.active : s.listIcon}>
+                <GridViewOutlinedIcon />
+              </div>
+              <p>Áttekintés</p>
+            </li>
+            <li className={s.sidebarListItem} onClick={() => setActualPage(2)}>
+              <div className={actualPage === 2 ? s.active : s.listIcon}>
+                <PermIdentityOutlinedIcon />
+              </div>
+              <p>Felhasználók</p>
+            </li>
+            <li className={s.sidebarListItem} onClick={() => setActualPage(3)}>
+              <div className={actualPage === 3 ? s.active : s.listIcon}>
+                <PaidOutlinedIcon />
+              </div>
+              <p>Jóváírások</p>
+            </li>
+            <li className={s.sidebarListItem} onClick={() => setActualPage(4)}>
+              <div className={actualPage === 4 ? s.active : s.listIcon}>
+                <LocalFireDepartmentOutlinedIcon />
+              </div>
+              <p>Népszerű</p>
+            </li>
+          </ul>
         </div>
+
+        <div className={s.main}>{renderMain()}</div>
+
+        <div></div>
       </div>
-    </div>
+    </>
   );
 };
 
