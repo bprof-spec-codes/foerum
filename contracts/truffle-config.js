520e083bd3ae5ffbb6afbf108862d2ebcbbd2fee
Roland Bogdan <bogdanroland07@gmail.com>
2022-05-03 15:35:53 +0200

    truffle config set for local development with ganache

--- contracts/truffle-config.js
+++ contracts/truffle-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   networks: {
     development: {
-      host: "localhost",
+      host: "127.0.0.1",
       port: 8545,
       network_id: "*", // Match any network id
       gas: 5000000
@@ -9,6 +9,7 @@ module.exports = {
   },
   compilers: {
     solc: {
+      version: "0.8.10",
       settings: {
         optimizer: {
           enabled: true, // Default: false
