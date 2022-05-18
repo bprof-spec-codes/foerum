1c1e7cd7bfda05936098ed32f9e65ef13ff6f55e
conoren <conoren@gmail.com>
2022-05-19 00:24:38 +0200

    removed logout button from header, again

--- front-end/src/components/Home/Header.tsx
+++ front-end/src/components/Home/Header.tsx
@@ -9,7 +9,7 @@ import jwt_decode from "jwt-decode";
 import { IconButton } from "@mui/material";
 import AddCircleOutlinedIcon from "@mui/icons-material/AddCircleOutlined";
 import axios from "../../axios";
-import {ethers} from 'ethers';
+import { ethers } from "ethers";
 
 export interface IHeaderProps extends StateProps, DispatchProps {}
 
@@ -21,7 +21,7 @@ const Header: FC<IHeaderProps> = (props) => {
 
   const [auth, setAuth] = useState<IAuth | null>(null);
 
-	const [defaultAccount, setDefaultAccount] = useState<String | null>(null);
+  const [defaultAccount, setDefaultAccount] = useState<String | null>(null);
   const [connStatus, setConnStatus] = useState<Boolean>(false);
 
   useEffect(() => {
@@ -33,38 +33,36 @@ const Header: FC<IHeaderProps> = (props) => {
 
     let _connStatus = getConnStatus();
     setConnStatus(_connStatus);
-
   }, [isAuthenticated, props]);
 
   const connectWalletHandler = () => {
-		if (window.ethereum && window.ethereum.isMetaMask) {
-
-			window.ethereum.request({ method: 'eth_requestAccounts'})
-			.then((result : any) => {
-        const userid = sessionStorage.getItem("userid");
-        const data = {address: result[0]};
-        axios.post("/MyUser/SetWallet/" + userid, data)
-        setDefaultAccount(result[0]);
-        setConnStatus(true);
-        sessionStorage.setItem("walletaddress", result[0]);
-			})
-			.catch((error : any) => {
-				console.log(error.message);
-			});
-		} else {
-      alert('Telepítsd fel a MetaMask kiegészítőt.');
-		}
-	}
+    if (window.ethereum && window.ethereum.isMetaMask) {
+      window.ethereum
+        .request({ method: "eth_requestAccounts" })
+        .then((result: any) => {
+          const userid = sessionStorage.getItem("userid");
+          const data = { address: result[0] };
+          axios.post("/MyUser/SetWallet/" + userid, data);
+          setDefaultAccount(result[0]);
+          setConnStatus(true);
+          sessionStorage.setItem("walletaddress", result[0]);
+        })
+        .catch((error: any) => {
+          console.log(error.message);
+        });
+    } else {
+      alert("Telepítsd fel a MetaMask kiegészítőt.");
+    }
+  };
 
   const getConnStatus = () => {
-    if(window.ethereum && window.ethereum.isMetaMask){
-      if(sessionStorage.getItem("walletaddress")) return true;
-      else return false; 
-    }
-    else{
+    if (window.ethereum && window.ethereum.isMetaMask) {
+      if (sessionStorage.getItem("walletaddress")) return true;
+      else return false;
+    } else {
       return false;
     }
-  }
+  };
   return (
     <div className="fixed w-full h-14 bg-basebg shadow-lg z-50">
       <div className="flex justify-between h-full mx-5 content-center text-center text-white">
@@ -85,13 +83,19 @@ const Header: FC<IHeaderProps> = (props) => {
               Admin felület
             </p>
           )}
-          {connStatus === false &&
-            <IconButton onClick={connectWalletHandler}><div className="border-2 px-2 py-2 rounded-full text-sm text-white"><AddCircleOutlinedIcon className="text-sm" />&nbsp;pénztárca csatlakoztatása&nbsp;</div></IconButton>
-          }
-          {connStatus === true &&
-            <div className="border-2 px-2 py-2 rounded-full text-sm text-white">&nbsp;csatlakozva&nbsp;</div>
-          }
-          {auth && <SignOutButton />}
+          {connStatus === false && (
+            <IconButton onClick={connectWalletHandler}>
+              <div className="border-2 px-2 py-2 rounded-full text-sm text-white">
+                <AddCircleOutlinedIcon className="text-sm" />
+                &nbsp;pénztárca csatlakoztatása&nbsp;
+              </div>
+            </IconButton>
+          )}
+          {connStatus === true && (
+            <div className="border-2 px-2 py-2 rounded-full text-sm text-white">
+              &nbsp;csatlakozva&nbsp;
+            </div>
+          )}
         </div>
       </div>
     </div>
