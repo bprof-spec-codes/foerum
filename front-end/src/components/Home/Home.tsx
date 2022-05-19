482fd072dfdaf77183f6a8cfdbff4923c187ec30
Boldi Bihari <boldibihari@gmail.com>
2022-05-19 23:09:22 +0200

    Tobics order by newest done

--- front-end/src/components/Home/Home.tsx
+++ front-end/src/components/Home/Home.tsx
@@ -153,6 +153,7 @@ const Home = () => {
                     t.subjectID === selectedSubject?.subjectID &&
                     t.topicName?.toLowerCase().includes(topicSearchKeyword)
                 )
+                .sort((a, b) => new Date(b.creationDate).getTime() - new Date(a.creationDate).getTime())
                 .map((topic, i) => (
                   <div key={i}>
                     {users && (
